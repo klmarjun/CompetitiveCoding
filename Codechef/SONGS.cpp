52795c7d1b42eab03b271a43f5e08a1db411763a
Arjun M <114378622+klmarjun@users.noreply.github.com>
2024-01-03 23:26:36 +0530

    Add files via upload

---
+++ Codechef/SONGS.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int iter,x,y;
+	cin>>iter;
+	for(int i=0;i<iter;i++){
+	    cin>>x>>y;
+	    cout<< floor(x/(y*3))<<endl;
+	}
+
+}
