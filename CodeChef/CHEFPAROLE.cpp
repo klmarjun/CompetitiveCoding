9c093dde6f10436396ed320d01823c1d279a682c
klmarjun <klmarjun@gmail.com>
2025-01-15 11:33:13 +0530

    DAY 15

---
+++ CodeChef/CHEFPAROLE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int days;
+    cin>>days;
+    if(days>=7){
+        cout<<"YES";
+    }
+    else{
+        cout<<"NO";
+    }
+}
