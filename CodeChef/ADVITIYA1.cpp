2b96068d48b49c0fdf7d2a4aa9bcf67f0091318a
klmarjun <klmarjun@gmail.com>
2025-01-17 23:17:29 +0530

    DAY 17

---
+++ CodeChef/ADVITIYA1.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int date;
+	cin>>date;
+	if(date<16){
+	    cout<<"WAITING FOR ADVITIYA";
+	}
+	else{
+	    cout<<"ADVITIYA";
+	}
+}
